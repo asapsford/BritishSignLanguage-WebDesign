5a223e4adba87bd7205a90fb0dc93f612a62d770
Alex Sapsford <asapsford11@gmail.com>
2020-05-01 15:49:47 +0100

    completed quiz java script and testing

--- validate-QUIZ.js
+++ validate-QUIZ.js
@@ -1,34 +1,38 @@
 
 function validate() {
  
-var userName = document.forms[0].username.value;
-var usernameError = "Please enter a correct syntax name \n";
-var question1 = document.forms[0].Question1.value;
+// Reference for JavaScript: https://www.w3schools.com/js/js_htmldom_document.asp
+
+var userName = document.forms[0].UserInfo.value;
+var usernameError = "* Please enter a correct syntax name * \n";
+var question1 = document.forms[0].Q1.value;
 var score; 
 
 var error=[];
-var question1Error = "Please answer the first question \n";
-var question2Error = "The answer requires 2 boxes to be checked";
-var question3Error= "Please answer the third question \n";
-var quesiton4Error= "Please answer the fourth question \n";
+var question1Error = "* Please answer the first question * \n";
+var question2Error = "* Question 2 answer requires 2 boxes to be checked * \n";
+var question3Error= "* Please answer the third question * \n";
+var question4Error= "* Please answer the fourth question * \n";
 
-var question2a = document.getElementById("q2a").checked;
-var question2b = document.getElementById("q2b").checked;
-var question2c = document.getElementById("q2c").checked;
-var question2d = document.getElementById("q2d").checked;
+var question2 = new Array();
+var question2a = document.getElementById("Q2a").checked;
+var question2b = document.getElementById("Q2b").checked;
+var question2c = document.getElementById("Q2c").checked;
+var question2d = document.getElementById("Q2d").checked;
 var question2Answer = 0;
 
-var question3 = document.forms[0].Question3.value;
+var question3 = document.forms[0].Q3.value;
 
-var question4 = document.forms[0].Question4.value;
+var question4 = document.forms[0].Q4.value;
 var question4Answer = "british, australian & new zealand sign language";
+var question4AltAnswer="british, australian and new zealand sign language";
 
 score = 0;
 
 /* Error check for null username */
 if(userName == ""){
     error.push(usernameError);
-    document.getElementById("username").style.backgroundColor = "yellow";
+    document.getElementById("UserInfo").style.backgroundColor = "yellow";
 }
 
 /* Question 1*/
@@ -38,13 +42,13 @@ if(question1 == "c"){
 }
 else if (question1 == ""){
 error.push(question1Error);
-document.getElementById("Question1").style.backgroundColor = "yellow";
+document.getElementById("Q1").style.backgroundColor = "yellow";
 }
 
 /* Question 2*/
 /* Loops through the answers to question 2, checks if it is the correct answers (n=2) and updates thte score if correct (a+c)*/
 
-var question2 = [question2a, question2b, question2c, question2d];
+question2 = [question2a, question2b, question2c, question2d]; // array to hold the question answers to loop through them to check if they are correct
 for(var i=0; i<question2.length; i++){
 
     if(question2[i] == true){
@@ -53,10 +57,19 @@ for(var i=0; i<question2.length; i++){
 }
 if(question2Answer < 2 || question2Answer > 2){
     error.push(question2Error);
-    document.getElementById("Question2").style.backgroundColor = "yellow";
+    document.getElementById("Q2").style.backgroundColor = "yellow";
 }
-else if(question2a == true|| question2c == true){
+else if(question2a == true ) {
     score++;
+ if(question2c == true){
+    score++;
+    }
+} 
+else if(question2c == true){
+    score++
+    if(question2a == true) {
+        score++;
+    }
 }
 
 /* Question 3*/
@@ -66,7 +79,7 @@ if(question3 == "b"){
 }
 else if(question3 ==""){
     error.push(question3Error);
-    document.getElementById("Question3").style.backgroundColor = "yellow";
+    document.getElementById("Q3").style.backgroundColor = "yellow";
 }
 
 /* Question 4*/
@@ -74,20 +87,23 @@ else if(question3 ==""){
 if (question4.toLowerCase() == question4Answer){
     score++;
 }
+else if(question4.toLowerCase() == question4AltAnswer){
+    score++;
+}
 else if(question4 == ""){
-    error.push(quesiton4Error);
-    document.getElementById("Question4").style.backgroundColor = "yellow";
+    error.push(question4Error);
+    document.getElementById("Q4").style.backgroundColor = "yellow";
 }
 
 /* Display alert of score to the user */
-if(error.length == 1){
+if(error.length === 0){
     document.forms[0].thisScore.value = score;
-    document.getElementById("Question1").style.backgroundColor = null;
-    document.getElementById("Question2").style.backgroundColor = null;
-    document.getElementById("Question3").style.backgroundColor = null;
-    document.getElementById("Question4").style.backgroundColor = null;
+    document.getElementById("Q1").style.backgroundColor = null;
+    document.getElementById("Q2").style.backgroundColor = null;
+    document.getElementById("Q3").style.backgroundColor = null;
+    document.getElementById("Q4").style.backgroundColor = null;
 
-    alert("Congratulations! Your score is: "+score);
+    alert("Congratulations! Your score is: " + score);
 }
 else{
     alert(error);
